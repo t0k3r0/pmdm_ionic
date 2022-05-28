f79afb9119b1fcc7ee940d084f061c83812d8b1f
Joseba Sanchez <josebsanchez@birt.eus>
2022-05-28 15:55:55 +0200

    initial commit

--- app/tab2/tab2.page.ts
+++ app/tab2/tab2.page.ts
@@ -6,7 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-
+  peso: number;
+  altura: number;
+  imc: number;
   constructor() {}
-
+  onClick() {
+    this.imc = this.peso * this.altura;
+}
 }
